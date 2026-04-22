7eda5286056f8b4ad8ce31d70325b51d1102e51f
mankda <mankda@greynexus.ai>
2026-04-22 08:41:08 +0530

    chore: add docusaurus scaffold to first-project

---
+++ projects/first-project/docusaurus.config.js
@@ -0,0 +1,27 @@
+// @ts-check
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'first-project',
+  url: 'http://localhost:3000',
+  baseUrl: '/docs/first-project/',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+  trailingSlash: false,
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        blog: false,
+        theme: { customCss: [] },
+      }),
+    ],
+  ],
+};
+
+module.exports = config;
